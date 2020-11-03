4c59bfd991f4918fc862b9123e7942751a8e9e4e
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-11-02 21:41:05 -0600

    List deploy keys

--- lib/Orbital/CLI/Command/GitHub/DeployKey.pm
+++ lib/Orbital/CLI/Command/GitHub/DeployKey.pm
@@ -5,12 +5,20 @@ package Orbital::CLI::Command::GitHub::DeployKey;
 use Moo;
 use CLI::Osprey;
 
-subcommand 'add' => sub {
-	...
-};
+subcommand 'add' => 'Orbital::CLI::Command::GitHub::DeployKey::Add';
 
-subcommand 'list' => sub {
-	...
+subcommand 'list' => method() {
+	my $github = $self->github_repos;
+	my $first_repo = $github->[0];
+	chomp( my $token = `git config --get github.token` );
+
+	my $keys_data = Pithub->new( token => $token )
+		->repos->keys->list(
+			user => $first_repo->namespace,
+			repo => $first_repo->name
+		);
+	say "Keys for: @{[ $first_repo->_github_slug ]}";
+	use Data::Dumper; print Dumper $keys_data->content;
 };
 
 with qw(Orbital::CLI::Command::Role::GitHubRepos);
---
+++ lib/Orbital/CLI/Command/GitHub/DeployKey/Add.pm
@@ -0,0 +1,22 @@
+use Orbital::Transfer::Common::Setup;
+package Orbital::CLI::Command::GitHub::DeployKey::Add;
+# ABSTRACT: Add repo deploy key
+
+use Moo;
+use CLI::Osprey;
+
+option deploy_key_path => (
+	is => 'ro',
+	format => 's',
+	required => 1,
+);
+
+method run(@) {
+	my $github = $self->github_repos;
+	my $first_repo = $github->[0];
+
+}
+
+with qw(Orbital::CLI::Command::Role::GitHubRepos);
+
+1;
